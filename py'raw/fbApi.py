4cdf604ca1ec55e0741e7e0ffdac520c87b83761
juneju-darad <djuneju@gmail.com>
2021-01-01 13:46:11 +0600

    fb api test

---
+++ py'raw/fbApi.py
@@ -0,0 +1,7 @@
+
+
+token = 'EAAGR4MbD6cgBALQ8YDl1jVQwA1LzqupR1KK0IuHjZCrTFBAYCupFGb49r215GT9e2T7ukBpBX4q1CtMFsCJRptJyFu8F9xbp4zgAXQvXtea7Bq79vsYrRSePEd3YKEVZBnXmaCMCIbbKcTaM8c3inCJVZC6b7ehsHutagSDbkSZClwcwyofYXIem9YbGPFNBY9gGgWdWhExEZADP2Pn17iKlaFy3j6lUxNW9RRZAK2kwZDZD'
+
+me = 'https://graph.facebook.com/v9.0/me?access_token=' + token
+f = 'https://graph.facebook.com/v9.0/me/friends?access_token=' + token
+
